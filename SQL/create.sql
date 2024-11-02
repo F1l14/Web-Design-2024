baf6b78e2c6890b8bdbc4e0af12b60b5892cf3a7
F1l14 <filipmn42@gmail.com>
2024-11-02 12:43:24 +0200

    initial files

---
+++ SQL/create.sql
@@ -0,0 +1,262 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 02, 2024 at 11:28 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `diploma`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `address`
+--
+DROP DATABASE IF EXISTS diploma;
+CREATE DATABASE diploma;
+USE diploma;
+
+CREATE TABLE `address` (
+  `am` int(7) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `street` varchar(255) NOT NULL,
+  `tk` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comission`
+--
+
+CREATE TABLE `comission` (
+  `diploma` int(7) NOT NULL,
+  `prof1` varchar(30) NOT NULL,
+  `prof2` varchar(30) NOT NULL,
+  `prof3` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comission_app`
+--
+
+CREATE TABLE `comission_app` (
+  `diploma` int(7) NOT NULL,
+  `professor` varchar(30) NOT NULL,
+  `datetime` datetime NOT NULL DEFAULT current_timestamp(),
+  `status` enum('accepted','rejected','waiting','') NOT NULL DEFAULT 'waiting'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `diploma`
+--
+
+CREATE TABLE `diploma` (
+  `id` int(7) NOT NULL,
+  `title` text NOT NULL,
+  `description` text NOT NULL,
+  `student` int(7) NOT NULL,
+  `url` text NOT NULL COMMENT 'text datatype to accept long urls without specifying the size limit',
+  `status` enum('active','processing','closed','cancelled') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `diploma_app`
+--
+
+CREATE TABLE `diploma_app` (
+  `diploma` int(7) NOT NULL,
+  `student` int(7) NOT NULL,
+  `datetime` int(11) NOT NULL DEFAULT current_timestamp(),
+  `status` enum('accepted','rejected','waiting','') NOT NULL DEFAULT 'waiting'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `evaluation`
+--
+
+CREATE TABLE `evaluation` (
+  `diploma` int(7) NOT NULL,
+  `datetime` datetime DEFAULT NULL,
+  `grade` decimal(4,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `professor`
+--
+
+CREATE TABLE `professor` (
+  `am` varchar(30) NOT NULL,
+  `tmhma` varchar(255) NOT NULL,
+  `status` enum('available','unavailable') NOT NULL DEFAULT 'available'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `am` int(7) NOT NULL,
+  `etos_eisagwghs` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `am` int(7) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `patrwnumo` varchar(255) NOT NULL,
+  `kinito` int(10) NOT NULL,
+  `stathero` int(10) NOT NULL,
+  `role` enum('professor','student','grammateia') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `address`
+--
+ALTER TABLE `address`
+  ADD PRIMARY KEY (`am`);
+
+--
+-- Indexes for table `comission`
+--
+ALTER TABLE `comission`
+  ADD PRIMARY KEY (`diploma`),
+  ADD KEY `fk_prof1` (`prof1`),
+  ADD KEY `fk_prof2` (`prof2`),
+  ADD KEY `fk_prof3` (`prof3`);
+
+--
+-- Indexes for table `comission_app`
+--
+ALTER TABLE `comission_app`
+  ADD PRIMARY KEY (`diploma`,`professor`),
+  ADD KEY `fk_prof_app` (`professor`);
+
+--
+-- Indexes for table `diploma`
+--
+ALTER TABLE `diploma`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_student` (`student`);
+
+--
+-- Indexes for table `diploma_app`
+--
+ALTER TABLE `diploma_app`
+  ADD PRIMARY KEY (`diploma`,`student`);
+
+--
+-- Indexes for table `evaluation`
+--
+ALTER TABLE `evaluation`
+  ADD PRIMARY KEY (`diploma`);
+
+--
+-- Indexes for table `professor`
+--
+ALTER TABLE `professor`
+  ADD PRIMARY KEY (`am`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`am`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`am`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `diploma`
+--
+ALTER TABLE `diploma`
+  MODIFY `id` int(7) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `address`
+--
+ALTER TABLE `address`
+  ADD CONSTRAINT `am` FOREIGN KEY (`am`) REFERENCES `users` (`am`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `comission`
+--
+ALTER TABLE `comission`
+  ADD CONSTRAINT `fk_diploma2` FOREIGN KEY (`diploma`) REFERENCES `diploma` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_prof1` FOREIGN KEY (`prof1`) REFERENCES `professor` (`am`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_prof2` FOREIGN KEY (`prof2`) REFERENCES `professor` (`am`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_prof3` FOREIGN KEY (`prof3`) REFERENCES `professor` (`am`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `comission_app`
+--
+ALTER TABLE `comission_app`
+  ADD CONSTRAINT `fk_diploma_app` FOREIGN KEY (`diploma`) REFERENCES `diploma` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_prof_app` FOREIGN KEY (`professor`) REFERENCES `professor` (`am`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `diploma`
+--
+ALTER TABLE `diploma`
+  ADD CONSTRAINT `fk_student` FOREIGN KEY (`student`) REFERENCES `student` (`am`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `evaluation`
+--
+ALTER TABLE `evaluation`
+  ADD CONSTRAINT `fk_diploma` FOREIGN KEY (`diploma`) REFERENCES `diploma` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
