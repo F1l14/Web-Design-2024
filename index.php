7bce2097ace5cfd7dc6cc25820ff184b49ec5452
F1l14 <filipmn42@gmail.com>
2024-11-12 17:07:37 +0200

    practice

--- index.php
+++ index.php
@@ -6,39 +6,14 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Hello World!</h1>
+    <h1 id="test">Hello World!</h1>
+    <button onclick="document.getElementById('test').innerHTML = Date()">Hello</button>
+
 </body>
 </html>
 
 
-<?php
-    include("dbconn.php");
-    $jsondata= file_get_contents("users.json");
-    $json = json_decode($jsondata, true);
-   
-    foreach ($json as $user) {
-        // Access the 'username' field correctly
-    //    echo $user['username'] . "<br>";
-    
-    
-        $insertUsers = $conn->prepare("INSERT INTO users (username, password, am, email, firstname, lastname, patrwnumo, kinito, stathero, role)
-                        VALUES (?, ?, ?, ?, ?, ? ,? ,? ,? ,?)");
-        $insertUsers->bind_param("ssissssiis", $username, $password, $am, $email, $firstname, $lastname, $patrwnumo, $kinito, $stathero, $role);
-        
-        $username = $user['username'];
-        $password = $user['password'];
-        $am = $user['am'];
-        $email = $user['email'];
-        $firstname = $user['firstname'];
-        $lastname = $user['lastname'];
-        $patrwnumo = $user['patrwnumo'];
-        $kinito = $user['kinito'];
-        $stathero = $user['stathero'];
-        $role = $user['role'];
-
-        $insertUsers->execute();
-    
-    }
-    $insertUsers->close();
-    $conn->close();
-?>
+<script>
+    let test = 3;
+    console.log(test);
+</script>
