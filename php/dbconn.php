63d3f0fb6adac216e01f37bafcd4145e861d9277
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2024-11-13 22:39:44 +0200

    rename of database

--- php/dbconn.php
+++ php/dbconn.php
@@ -2,7 +2,7 @@
     $db_server = "localhost";
     $db_user = "root";
     $db_pass = "";
-    $db_name = "ThesisDB";
+    $db_name = "thesisdb";
     
     // Try to establish a connection
     $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
