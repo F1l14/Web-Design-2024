c83ddf64fa2c3f28eb6e7621bd472b88b9bf32f2
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2024-11-14 22:44:13 +0200

    added css to login
    Co-authored-by: Filippos Minopetros <F1l14@users.noreply.github.com>

--- index.php
+++ index.php
@@ -3,25 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
     <title>Document</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
 </head>
 <body>
 <form id="loginForm" action="<?php echo htmlspecialchars('https://' . $_SERVER['HTTP_HOST'] . '/Web-Design-2024/php/index/login.php'); ?>" method="POST">
 
-    <label for="username">Username<br></label>
-    <input id="userName" type="text" name="username" placeholder="Test Name" maxlength="50">
+    <!-- <label for="username" class="loginText">Username<br></label> -->
+    <input id="userName" class="loginText" type="text" name="username" placeholder="Username" maxlength="50">
     <br>
 
 
-    <label for="pass">Password<br></label>
-    <input id="passWord" type="password" name="password" placeholder="test" maxlength="50">
+    <!-- <label for="pass" class="loginText">Password<br></label> -->
+    <input id="passWord" class="loginText" type="password" name="password" placeholder="Password" maxlength="50">
 
-    <input type="reset" value="Clear">
     <button type="submit" name="login">Login</button>
     
 
     </form>
-    <p id="loginError">test</p>
+    <p id="loginError"></p>
 
 
 </body>
