603acd9ec8d40b9f9b29493f5850cecfd54c49ee
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2024-11-16 12:00:05 +0200

    added header
    Co-authored-by: Filippos Minopetros <F1l14@users.noreply.github.com>

--- index.php
+++ index.php
@@ -9,6 +9,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     
 </head>
+
+<header>
+  <?php include("header.html")?>
+</header>
+
 <body>
     <div class="row justify-content-center">
         <div class="col-sm-6 col-md-3 col-lg-3">
@@ -50,7 +55,7 @@
         selector: '.background',
         color: ["#33c8ff","#000000"],
         connectParticles: true,
-        speed: 0.1,
+        speed: 0.05,
         responsive: [
     {
       breakpoint: 425,
