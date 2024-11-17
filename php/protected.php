51ee0967cdbfe3a5a6eb2df36dca55ad662f1be0
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2024-11-17 17:57:53 +0200

    redirects to login if not valid and added comments for the next step

--- php/protected.php
+++ php/protected.php
@@ -4,13 +4,47 @@ require("validateToken.php");
 $data = json_decode(validateToken());
 
 if ($data->response !== 'valid') {
-    die(json_encode([
-        "error" => "Invalid response value",
-    ]));
+    echo "invalid HERE ";
+    header("Location: https://localhost/Web-Design-2024/");
+    exit();
+    // die(json_encode([
+    //     "error" => "Invalid response value",
+    // ]));
+   
 }
+// if ($data->role == "student") {
+//     header("Location: student");
+// }
+
+// if ($data->role == "professor") {
+//     header("Location: professor");
+// }
+
+// if ($data->role == "grammateia") {
+//     header("Location: grammateia");
+// }
 
 // Continue processing...
 echo "Response is " . $data->response . "\n";
 echo "Welcome " . $data->user . "\n";
 echo "My role is " . $data->role . "\n";
+
 ?>
+
+<!-- 
+[in professorProtectedPage.php] protected("professor")
+
+
+
+function protected (protectedPageAccess){
+    if(validateToken){
+        
+        if(MyToken->Role != protectedPageAccess)
+        {
+            header(MyRole/startPage.php)
+        }
+
+    }else{
+        header(index.php)
+    }
+} -->
