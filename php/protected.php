4c054e34e6b3e47260a1b36603eaa068ed56efb1
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2024-11-17 17:00:39 +0200

    removed error checking lines
    Co-authored-by: Filippos Minopetros <F1l14@users.noreply.github.com>

--- php/protected.php
+++ php/protected.php
@@ -2,21 +2,6 @@
 require("validateToken.php");
 
 $data = json_decode(validateToken());
-// Check if decoding failed
-if ($data === null) {
-    echo "data is null";
-    die(json_encode([
-        "error" => "Invalid JSON payload",
-        "details" => json_last_error_msg(),  // Provide the specific error from json_decode
-    ]));
-}
-
-// Check if 'response' exists and if it has the correct value
-if (!isset($data->response)) {
-    die(json_encode([
-        "error" => "Missing 'response' key in the JSON payload",
-    ]));
-}
 
 if ($data->response !== 'valid') {
     die(json_encode([
