7cd7a144bcf46151b0478d0fe0095b865a529aff
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2024-11-19 00:39:16 +0200

    uses is connected

--- index.php
+++ index.php
@@ -11,12 +11,19 @@
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <!-- icon -->
   <link rel="icon" type="image/svg+xml" href="icons/websiteIcon.svg ">
-  <?php include("header.html") ?>
+
+  <?php include "header.html";
+        include "./php/index/isConnected.php";
+  ?>
 
 </head>
 
 
 <body>
+<div class="spinner-border text-primary" role="status">
+  <span class="visually-hidden">Loading...</span>
+</div>
+
   <div id="mainContainer" class="container align-content-center">
     <div class="row  justify-content-between ">
 
@@ -160,20 +167,21 @@
 
         </form>
       </div>
-
-
     </div>
   </div>
   <canvas class="background"></canvas>
-  <!-- bootstrap -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+</body>
+
+</html>
+
+<!-- bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
     crossorigin="anonymous"></script>
   <!-- bacgkround lib -->
   <script src=".\particles.js-master\particles.js-master\dist\particles.min.js"></script>
-</body>
 
-</html>
+
 
 <!-- login handler -->
 <script src="./js/index/handleLogin.js"></script>
