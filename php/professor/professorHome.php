a0513b16cbdd169cf1a71e2234927c75dbe4111a
F1l14 <filipmn42@gmail.com>
2024-11-19 19:54:14 +0200

    user header and profile dropdown initial

--- php/professor/professorHome.php
+++ php/professor/professorHome.php
@@ -1,5 +1,39 @@
 <?php
-require("../tokenFunctions.php");
+require_once("../tokenFunctions.php");
 roleProtected("professor");
 updateActivity();
-echo "Welcome Professor";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>ΣΔΔΕ</title>
+    <link rel="stylesheet" href="../../css/index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- icon -->
+    <link rel="icon" type="image/svg+xml" href="icons/websiteIcon.svg ">
+    <?php include("../../header2.html") ?>
+
+</head>
+
+<body>
+
+    <div id="mainContainer" class="container align-content-center">
+
+    </div>
+
+    <!-- ======================================================================================== -->
+    <canvas class="background"></canvas>
+</body>
+
+<!-- bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<!-- bacgkround lib -->
+<script src="..\..\particles.js-master\particles.js-master\dist\particles.min.js"></script>
+<script src="../../js/backgroundOptions.js"></script>
