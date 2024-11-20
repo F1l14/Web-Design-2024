cc500970d74c2229d1f29314d71a90efcd005aca
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2024-11-20 16:47:35 +0200

    added buttons and funtion pages with php

--- php/student/studentHome.php
+++ php/student/studentHome.php
@@ -2,4 +2,62 @@
 require("../tokenFunctions.php");
 roleProtected("student");
 updateActivity();
-echo "Welcome Student";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>ΣΔΔΕ</title>
+    <link rel="stylesheet" href="/Web-Design-2024/css/styles.css">
+    <link rel="stylesheet" href="/Web-Design-2024/css/header.css">
+    <link rel="stylesheet" href="/Web-Design-2024/css/homepage.css">
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- icon -->
+    <link rel="icon" type="image/svg+xml" href="/Web-Design-2024/icons/websiteIcon.svg ">
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/Web-Design-2024/header2.html' ?>
+</head>
+
+<body>
+    <div id="mainContainer" class="container align-content-center ">
+        <div class="buttonWindow box align-content-center">
+            <div id="innerContainer" class="container text-center">
+                <div class="row row-cols-3 ">
+                    <div class="col">
+                        <button class="pageButton" onclick="redirect('viewThesis.php')">Προβολή Θέματος</button>
+                    </div>
+                    <div class="col">
+                        <button class="pageButton" onclick="redirect('profile.php')">Επεξεργασία Προφίλ</button>
+                    </div>
+                    <div class="col">
+                        <button class="pageButton" onclick="redirect('manageThesis.php')">Διαχείρηση Διπλωματικής Εργασίας</button>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <!-- ======================================================================================== -->
+    <canvas class="background"></canvas>
+</body>
+
+<!-- bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+<!-- bacgkround lib -->
+<script src="..\..\particles.js-master\particles.js-master\dist\particles.min.js"></script>
+<script src="../../js/backgroundOptions.js"></script>
+<script>
+    function redirect(url) {
+        window.location.href = url;
+    }
+</script>
