e9ddb80c7155c68e9778115dc06efd8fec4edd77
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2024-11-23 18:22:46 +0200

    updated database
    Co-authored-by: Filippos Minopetros <100228368+F1l14@users.noreply.github.com>

--- SQL/thesisdb.sql
+++ SQL/thesisdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 23, 2024 at 12:11 AM
+-- Generation Time: Nov 23, 2024 at 05:22 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -30,9 +30,45 @@ SET time_zone = "+00:00";
 CREATE TABLE `address` (
   `username` varchar(30) NOT NULL,
   `city` varchar(100) NOT NULL,
-  `street` int(100) NOT NULL,
+  `street` varchar(100) NOT NULL,
+  `number` int(11) NOT NULL,
   `zipcode` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `address`
+--
+
+INSERT INTO `address` (`username`, `city`, `street`, `number`, `zipcode`) VALUES
+('up0000001', 'Patras', 'Karaoli & Dimitriou', 5, 26222),
+('up0000002', 'Patras', 'Karaoli & Dimitriou', 5, 26222),
+('up0000003', 'Patras', 'Karaoli & Dimitriou', 5, 26222),
+('up0000004', 'Patras', 'Karaoli & Dimitriou', 5, 26222),
+('up0000005', 'Patras', 'Karaoli & Dimitriou', 5, 26222),
+('up0000006', 'Patras', 'Karaoli & Dimitriou', 5, 26222),
+('up0000007', 'Patras', 'Karaoli & Dimitriou', 5, 26222),
+('up0000008', 'Patras', 'Karaoli & Dimitriou', 5, 26222),
+('up0000009', 'Patras', 'Karaoli & Dimitriou', 5, 26222),
+('up0000011', 'Patras', 'Othonos-Amalias', 123, 26221),
+('up0000012', 'Patras', 'Othonos-Amalias', 123, 26221),
+('up0000013', 'Patras', 'Othonos-Amalias', 123, 26221),
+('up0000014', 'Patras', 'Othonos-Amalias', 123, 26221),
+('up0000015', 'Patras', 'Othonos-Amalias', 123, 26221),
+('up0000016', 'Patras', 'Othonos-Amalias', 123, 26221),
+('up0000017', 'Patras', 'Othonos-Amalias', 123, 26221),
+('up0000018', 'Patras', 'Othonos-Amalias', 123, 26221),
+('up0000019', 'Patras', 'Othonos-Amalias', 123, 26221),
+('up0000020', 'Patras', 'Othonos-Amalias', 123, 26221),
+('up0000021', 'Patras', 'Othonos-Amalias', 123, 26221),
+('up0000022', 'Patras', 'Othonos-Amalias', 123, 26221),
+('up0000023', 'Patras', 'Othonos-Amalias', 123, 26221),
+('up0000024', 'Patras', 'Othonos-Amalias', 123, 26221),
+('up0000025', 'Patras', 'Othonos-Amalias', 123, 26221),
+('up0000026', 'Patras', 'Othonos-Amalias', 123, 26221),
+('up0000027', 'Patras', 'Othonos-Amalias', 123, 26221),
+('up0000028', 'Patras', 'Othonos-Amalias', 123, 26221),
+('up0000029', 'Patras', 'Othonos-Amalias', 123, 26221),
+('up0000030', 'Patras', 'Othonos-Amalias', 123, 26221);
 
 -- --------------------------------------------------------
 
@@ -50,28 +86,6 @@ CREATE TABLE `diplomatiki` (
   `status` enum('energi','epeksergasia','peratomeni','akiromeni','anathesi','diathesimi') NOT NULL DEFAULT 'diathesimi',
   `filename` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `diplomatiki`
---
-
-INSERT INTO `diplomatiki` (`id`, `title`, `description`, `professor`, `student`, `url`, `status`, `filename`) VALUES
-(31, 'dfadsf', 'fadsfadsf', 'up0000001', NULL, NULL, 'diathesimi', 'Diplomatiki Website.pdf'),
-(32, 'fadsfadsf', 'fadsfdasf', 'up0000001', NULL, NULL, 'diathesimi', 'Diplomatiki Website.pdf'),
-(33, 'fdasfads', 'fdsafdsf', 'up0000001', NULL, NULL, 'diathesimi', 'Diplomatiki Website.pdf'),
-(34, 'fadsfads', 'fdsfasdfasf', 'up0000001', NULL, NULL, 'diathesimi', 'Diplomatiki Website.pdf'),
-(35, 'fdasfsa', 'fadsfdasf', 'up0000001', NULL, NULL, 'diathesimi', 'Diplomatiki Website.pdf'),
-(36, 'fdasfsa', 'fadsfdasf', 'up0000001', NULL, NULL, 'diathesimi', 'Diplomatiki Website.pdf'),
-(37, 'dasfadsf', 'fadsfsdf', 'up0000001', NULL, NULL, 'diathesimi', 'Diplomatiki Website.pdf'),
-(38, 'fadsfadsf', 'dasfasfdasf', 'up0000001', NULL, NULL, 'diathesimi', 'Diplomatiki Website.pdf'),
-(39, 'gfdsg', 'gsfdg', 'up0000001', NULL, NULL, 'diathesimi', 'Diplomatiki Website.pdf'),
-(40, 'f', 'f', 'up0000001', NULL, NULL, 'diathesimi', 'Diplomatiki Website.pdf'),
-(41, 'fadsfads', 'fadsfadsf', 'up0000001', NULL, NULL, 'diathesimi', 'Diplomatiki Website.pdf'),
-(42, 'fdsafa', 'dasfd', 'up0000001', NULL, NULL, 'diathesimi', 'Diplomatiki Website.pdf'),
-(43, 'fadsfads', 'fadsfasdf', 'up0000001', NULL, NULL, 'diathesimi', 'Diplomatiki Website.pdf'),
-(44, 'dfadsf', 'dsafads', 'up0000001', NULL, NULL, 'diathesimi', 'Diplomatiki Website.pdf'),
-(45, 'fddads', 'fdasf', 'up0000001', NULL, NULL, 'diathesimi', 'Diplomatiki Website.pdf'),
-(46, 'adsf', 'fadsf', 'up0000001', NULL, NULL, 'diathesimi', 'Diplomatiki Website.pdf');
 
 -- --------------------------------------------------------
 
@@ -133,6 +147,8 @@ CREATE TABLE `evaluation` (
 CREATE TABLE `professor` (
   `username` varchar(30) NOT NULL,
   `tmhma` varchar(255) NOT NULL,
+  `panepistimio` varchar(255) NOT NULL,
+  `thema` text NOT NULL,
   `status` enum('available','unavailable') NOT NULL DEFAULT 'available'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -140,8 +156,16 @@ CREATE TABLE `professor` (
 -- Dumping data for table `professor`
 --
 
-INSERT INTO `professor` (`username`, `tmhma`, `status`) VALUES
-('up0000001', 'CEID', 'available');
+INSERT INTO `professor` (`username`, `tmhma`, `panepistimio`, `thema`, `status`) VALUES
+('up0000001', 'Computer Science', 'University of Patras', 'Artificial Intelligence', 'available'),
+('up0000002', 'Computer Science', 'University of Patras', 'Artificial Intelligence', 'available'),
+('up0000003', 'Computer Science', 'University of Patras', 'Artificial Intelligence', 'available'),
+('up0000004', 'Computer Science', 'University of Patras', 'Artificial Intelligence', 'available'),
+('up0000005', 'Computer Science', 'University of Patras', 'Artificial Intelligence', 'available'),
+('up0000006', 'Computer Science', 'University of Patras', 'Artificial Intelligence', 'available'),
+('up0000007', 'Computer Science', 'University of Patras', 'Artificial Intelligence', 'available'),
+('up0000008', 'Computer Science', 'University of Patras', 'Artificial Intelligence', 'available'),
+('up0000009', 'Computer Science', 'University of Patras', 'Artificial Intelligence', 'available');
 
 -- --------------------------------------------------------
 
@@ -153,6 +177,32 @@ CREATE TABLE `student` (
   `username` varchar(30) NOT NULL,
   `etos_eisagwghs` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`username`, `etos_eisagwghs`) VALUES
+('up0000011', 2015),
+('up0000012', 2015),
+('up0000013', 2015),
+('up0000014', 2015),
+('up0000015', 2015),
+('up0000016', 2015),
+('up0000017', 2015),
+('up0000018', 2015),
+('up0000019', 2015),
+('up0000020', 2015),
+('up0000021', 2015),
+('up0000022', 2015),
+('up0000023', 2015),
+('up0000024', 2015),
+('up0000025', 2015),
+('up0000026', 2015),
+('up0000027', 2015),
+('up0000028', 2015),
+('up0000029', 2015),
+('up0000030', 2015);
 
 -- --------------------------------------------------------
 
@@ -168,8 +218,8 @@ CREATE TABLE `users` (
   `firstname` varchar(50) NOT NULL,
   `lastname` varchar(100) NOT NULL,
   `patrwnumo` varchar(50) NOT NULL,
-  `kinito` int(10) NOT NULL,
-  `stathero` int(10) NOT NULL,
+  `kinito` varchar(10) NOT NULL,
+  `stathero` varchar(10) NOT NULL,
   `role` enum('student','professor','grammateia') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -178,36 +228,36 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`username`, `password`, `am`, `email`, `firstname`, `lastname`, `patrwnumo`, `kinito`, `stathero`, `role`) VALUES
-('up0000001', '123', 1, 'up0000001@ac.upatras.gr', 'Αναστάσιος', 'Παπαδόπουλος', 'Γιώργος', 2147483647, 2109111111, 'professor'),
-('up0000002', '$2y$10$juPLpyDdhun3GmYPzPmH3eThszl.dHyIDQ0hZ9dcjYe6GUzsiKvbG', 2, 'up0000002@ac.upatras.gr', 'Μαρία', 'Κωστόπουλος', 'Αλέξανδρος', 2147483647, 2109222222, 'professor'),
-('up0000003', '$2y$10$ZmoKd8vAOcgvBmWVLUnmdu61cvt.2jWdwUjSp/vX4jPLNY/JyJeoy', 3, 'up0000003@ac.upatras.gr', 'Γιάννης', 'Στασινόπουλος', 'Χρήστος', 2147483647, 2109333333, 'professor'),
-('up0000004', '$2y$10$djDD62FTDzVWmlCXwtFFXOwGWgRzybusm7qujQtMzysKXOaORv80G', 4, 'up0000004@ac.upatras.gr', 'Δημήτρης', 'Αναγνωστόπουλος', 'Σταύρος', 2147483647, 2109444444, 'professor'),
-('up0000005', '$2y$10$CgE3aHTDNTvBpsug4v2IY.BUC866tNzVEBBQUZ0CD2mu0YMJ5K4VK', 5, 'up0000005@ac.upatras.gr', 'Αλέξανδρος', 'Παντελίδης', 'Κωνσταντίνος', 2147483647, 2109555555, 'professor'),
-('up0000006', '$2y$10$T6/WdH7z82rbR5L0XHvrMOapUkzLy1xLZpGHPhRwlzPHYh8S6PKs6', 6, 'up0000006@ac.upatras.gr', 'Σταύρος', 'Καραγιάννης', 'Πέτρος', 2147483647, 2109666666, 'professor'),
-('up0000007', '$2y$10$XDAaaSAWQY7T3wFQu7ydq.o3/QKSzLVCz3r70y1hG3LTChdKgLa1.', 7, 'up0000007@ac.upatras.gr', 'Ευάγγελος', 'Παπακωνσταντίνου', 'Αλέξανδρος', 2147483647, 2109777777, 'professor'),
-('up0000008', '$2y$10$CLjHf1thkRFNeY1zYvtzk.tHnwvvr3TLTx.a7kxPTqJqih8o0W97S', 8, 'up0000008@ac.upatras.gr', 'Κωνσταντίνος', 'Αβραάμ', 'Αντώνιος', 2147483647, 2109888888, 'professor'),
-('up0000009', '$2y$10$H5cRAKkZ4OdmLyEHW3msDO8Z8A0Cv6N5gYl8U0GXHHbZIsiYzimu2', 9, 'up0000009@ac.upatras.gr', 'Ανδρέας', 'Κυριακίδης', 'Στέφανος', 2147483647, 2109999999, 'professor'),
-('up0000010', '789', 10, 'up0000010@ac.upatras.gr', 'Νικόλαος', 'Κωσταρίδης', 'Γεώργιος', 2147483647, 2101234567, 'grammateia'),
-('up0000011', '456', 11, 'up0000011@ac.upatras.gr', 'Θεόφιλος', 'Κυριακίδης', 'Χρήστος', 2147483647, 2109112345, 'student'),
-('up0000012', '$2y$10$utff.mWuz3UXq2hdUsCbj.YHE7Chv7wPUEsdMTMWWVi7osECUq/d.', 12, 'up0000012@ac.upatras.gr', 'Αντώνιος', 'Παπαθεοδωρίδης', 'Δημήτρης', 2147483647, 2109212345, 'student'),
-('up0000013', '$2y$10$RP9B7UQZe1A4BT2b5gAqS.4Vf5XpOtQGOiXXotI1yX9/20TJ6ltTe', 13, 'up0000013@ac.upatras.gr', 'Γεώργιος', 'Αδαμίδης', 'Παναγιώτης', 2147483647, 2109312345, 'student'),
-('up0000014', '$2y$10$vG12Z5wu8xu9MCrsvugb/.SuUFYu.lGe2LuJPyxQtZVmMqWtbGv5m', 14, 'up0000014@ac.upatras.gr', 'Βασίλειος', 'Φωτόπουλος', 'Σωτήρης', 2147483647, 2109412345, 'student'),
-('up0000015', '$2y$10$8rW2Ps9jRgPCNstMUvh4zOBMFSyEm1Yl0O6CE5YcZ8oWXb10FYFVK', 15, 'up0000015@ac.upatras.gr', 'Χρήστος', 'Αντωνίου', 'Δημήτρης', 2147483647, 2109512345, 'student'),
-('up0000016', '$2y$10$v5rVBlSehSwV/OI043Qu4.J1X38IVC/y8c0fpkQHK4kw7rkuePZKW', 16, 'up0000016@ac.upatras.gr', 'Νίκος', 'Πασχαλίδης', 'Αριστείδης', 2147483647, 2109612345, 'student'),
-('up0000017', '$2y$10$lCzdJkCM5/vFtTuw1awknODX35EkGuS7YvGlVbN0QCW5hnCsCSvMi', 17, 'up0000017@ac.upatras.gr', 'Γιώργος', 'Κωνσταντίνου', 'Χρήστος', 2147483647, 2109712345, 'student'),
-('up0000018', '$2y$10$cf2nTC0XrU1j.EB5j7tqcO7TMnNamsAmXhARH1Eq8wXzlJpFqQ3oG', 18, 'up0000018@ac.upatras.gr', 'Ελένη', 'Στεφανάκη', 'Παναγιώτης', 2147483647, 2109812345, 'student'),
-('up0000019', '$2y$10$FEOjnc6H7NhfWLskSXd0we3Og5BNsFg6icZgMK.f85EppFZ43iXHq', 19, 'up0000019@ac.upatras.gr', 'Ανδρέας', 'Κατσούλης', 'Σπύρος', 2147483647, 2109912345, 'student'),
-('up0000020', '$2y$10$q.NkHsCj3z5Ej1gtp8xbweV9Go/SnS.CldJyfcPlSpxgGuWpgExRS', 20, 'up0000020@ac.upatras.gr', 'Μαρία', 'Βαβούρης', 'Δημήτρης', 2147483647, 2109412345, 'student'),
-('up0000021', '$2y$10$bWjU.92tu8o/IcRTLhojR.4az.p.ql/IeQF15TpBxxKEv146ptLOW', 21, 'up0000021@ac.upatras.gr', 'Παναγιώτης', 'Καραφωτιάς', 'Σταύρος', 2147483647, 2109512345, 'student'),
-('up0000022', '$2y$10$TayJa3ieAuFQjRn/zupCTOTJOCgFRthFUQOxB7IGBcfxc7obiCzCa', 22, 'up0000022@ac.upatras.gr', 'Γιάννης', 'Κωστόπουλος', 'Γιώργος', 2147483647, 2109612345, 'student'),
-('up0000023', '$2y$10$YIs38KF9CQ93EngpFb/Aye8ljqzDcS7yva0yMiQOYN0rigsQTcb4C', 23, 'up0000023@ac.upatras.gr', 'Φώτης', 'Κουτσογιάννης', 'Στέφανος', 2147483647, 2109712345, 'student'),
-('up0000024', '$2y$10$4/9et1yp2drXskRITU/ZmODsV6R4qU0a/rDnomwvdmP0ZmZZvTpZe', 24, 'up0000024@ac.upatras.gr', 'Νίκος', 'Πιλάτος', 'Αριστείδης', 2147483647, 2109812345, 'student'),
-('up0000025', '$2y$10$jLr3VkTb4Y1c/m0sRSmRo.r3G9f1XuM3LXhc6xmDkATg4NOgjpsUC', 25, 'up0000025@ac.upatras.gr', 'Χρήστος', 'Καλαφάτης', 'Στέφανος', 2147483647, 2109912345, 'student'),
-('up0000026', '$2y$10$NwOIrVYjWGBuuEFjDKjq.ex0cVs9zmBuuuyK64bLYbUYBujy6n2nS', 26, 'up0000026@ac.upatras.gr', 'Νίκος', 'Τσολάκης', 'Δημήτρης', 2147483647, 2109912345, 'student'),
-('up0000027', '$2y$10$X2eMrbJ6NQx9.Mvfs/IQtup0Z47gEfH7IxNCqLRlBYlT0ID3kJVN.', 27, 'up0000027@ac.upatras.gr', 'Δημήτρης', 'Φωτόπουλος', 'Αλέξανδρος', 2147483647, 2109512345, 'student'),
-('up0000028', '$2y$10$2n1.xlXcqkIMBdBSlWLCre8N2c1fH8NRG1I1QTFqEdLh2b53P0F9y', 28, 'up0000028@ac.upatras.gr', 'Κωνσταντίνος', 'Αντωνόπουλος', 'Γεώργιος', 2147483647, 2109612345, 'student'),
-('up0000029', '$2y$10$PIvJ3eJ/xwWw3hbiwyVm9eUcyFR1O8F73EcF8Jt9.CmZSm/J1V0WS', 29, 'up0000029@ac.upatras.gr', 'Γιώργος', 'Κοκκίνης', 'Νικόλαος', 2147483647, 2109712345, 'student'),
-('up0000030', '$2y$10$eeht7HTGFUPUwMg7il.n5u/WgWlHbBCYo6wwpEMJhPi9dTNRBPC.i', 30, 'up0000030@ac.upatras.gr', 'Αντώνης', 'Μητσούλης', 'Σπύρος', 2147483647, 2109812345, 'student');
+('grammateia', '789', 69, 'grammateia@ceid.upatras.gr', 'Κωνσταντίνος', 'Αχλάδης', 'Γεώργιος', '6975464452', '2610865974', 'grammateia'),
+('up0000001', '123', 1, 'up0000001@ac.upatras.gr', 'Αναστάσιος', 'Παπαδόπουλος', 'Γιώργος', '6901234567', '2109111111', 'professor'),
+('up0000002', '$2y$10$juPLpyDdhun3GmYPzPmH3eThszl.dHyIDQ0hZ9dcjYe6GUzsiKvbG', 2, 'up0000002@ac.upatras.gr', 'Μαρία', 'Κωστόπουλος', 'Αλέξανδρος', '6902234567', '2109222222', 'professor'),
+('up0000003', '$2y$10$ZmoKd8vAOcgvBmWVLUnmdu61cvt.2jWdwUjSp/vX4jPLNY/JyJeoy', 3, 'up0000003@ac.upatras.gr', 'Γιάννης', 'Στασινόπουλος', 'Χρήστος', '6903234567', '2109333333', 'professor'),
+('up0000004', '$2y$10$djDD62FTDzVWmlCXwtFFXOwGWgRzybusm7qujQtMzysKXOaORv80G', 4, 'up0000004@ac.upatras.gr', 'Δημήτρης', 'Αναγνωστόπουλος', 'Σταύρος', '6904234567', '2109444444', 'professor'),
+('up0000005', '$2y$10$CgE3aHTDNTvBpsug4v2IY.BUC866tNzVEBBQUZ0CD2mu0YMJ5K4VK', 5, 'up0000005@ac.upatras.gr', 'Αλέξανδρος', 'Παντελίδης', 'Κωνσταντίνος', '6905234567', '2109555555', 'professor'),
+('up0000006', '$2y$10$T6/WdH7z82rbR5L0XHvrMOapUkzLy1xLZpGHPhRwlzPHYh8S6PKs6', 6, 'up0000006@ac.upatras.gr', 'Σταύρος', 'Καραγιάννης', 'Πέτρος', '6906234567', '2109666666', 'professor'),
+('up0000007', '$2y$10$XDAaaSAWQY7T3wFQu7ydq.o3/QKSzLVCz3r70y1hG3LTChdKgLa1.', 7, 'up0000007@ac.upatras.gr', 'Ευάγγελος', 'Παπακωνσταντίνου', 'Αλέξανδρος', '6907234567', '2109777777', 'professor'),
+('up0000008', '$2y$10$CLjHf1thkRFNeY1zYvtzk.tHnwvvr3TLTx.a7kxPTqJqih8o0W97S', 8, 'up0000008@ac.upatras.gr', 'Κωνσταντίνος', 'Αβραάμ', 'Αντώνιος', '6908234567', '2109888888', 'professor'),
+('up0000009', '$2y$10$H5cRAKkZ4OdmLyEHW3msDO8Z8A0Cv6N5gYl8U0GXHHbZIsiYzimu2', 9, 'up0000009@ac.upatras.gr', 'Ανδρέας', 'Κυριακίδης', 'Στέφανος', '6909234567', '2109999999', 'professor'),
+('up0000011', '456', 11, 'up0000011@ac.upatras.gr', 'Θεόφιλος', 'Κυριακίδης', 'Χρήστος', '6912234567', '2109112345', 'student'),
+('up0000012', '$2y$10$utff.mWuz3UXq2hdUsCbj.YHE7Chv7wPUEsdMTMWWVi7osECUq/d.', 12, 'up0000012@ac.upatras.gr', 'Αντώνιος', 'Παπαθεοδωρίδης', 'Δημήτρης', '6913234567', '2109212345', 'student'),
+('up0000013', '$2y$10$RP9B7UQZe1A4BT2b5gAqS.4Vf5XpOtQGOiXXotI1yX9/20TJ6ltTe', 13, 'up0000013@ac.upatras.gr', 'Γεώργιος', 'Αδαμίδης', 'Παναγιώτης', '6914234567', '2109312345', 'student'),
+('up0000014', '$2y$10$vG12Z5wu8xu9MCrsvugb/.SuUFYu.lGe2LuJPyxQtZVmMqWtbGv5m', 14, 'up0000014@ac.upatras.gr', 'Βασίλειος', 'Φωτόπουλος', 'Σωτήρης', '6915234567', '2109412345', 'student'),
+('up0000015', '$2y$10$8rW2Ps9jRgPCNstMUvh4zOBMFSyEm1Yl0O6CE5YcZ8oWXb10FYFVK', 15, 'up0000015@ac.upatras.gr', 'Χρήστος', 'Αντωνίου', 'Δημήτρης', '6916234567', '2109512345', 'student'),
+('up0000016', '$2y$10$v5rVBlSehSwV/OI043Qu4.J1X38IVC/y8c0fpkQHK4kw7rkuePZKW', 16, 'up0000016@ac.upatras.gr', 'Νίκος', 'Πασχαλίδης', 'Αριστείδης', '6917234567', '2109612345', 'student'),
+('up0000017', '$2y$10$lCzdJkCM5/vFtTuw1awknODX35EkGuS7YvGlVbN0QCW5hnCsCSvMi', 17, 'up0000017@ac.upatras.gr', 'Γιώργος', 'Κωνσταντίνου', 'Χρήστος', '6918234567', '2109712345', 'student'),
+('up0000018', '$2y$10$cf2nTC0XrU1j.EB5j7tqcO7TMnNamsAmXhARH1Eq8wXzlJpFqQ3oG', 18, 'up0000018@ac.upatras.gr', 'Ελένη', 'Στεφανάκη', 'Παναγιώτης', '6919234567', '2109812345', 'student'),
+('up0000019', '$2y$10$FEOjnc6H7NhfWLskSXd0we3Og5BNsFg6icZgMK.f85EppFZ43iXHq', 19, 'up0000019@ac.upatras.gr', 'Ανδρέας', 'Κατσούλης', 'Σπύρος', '6920234567', '2109912345', 'student'),
+('up0000020', '$2y$10$q.NkHsCj3z5Ej1gtp8xbweV9Go/SnS.CldJyfcPlSpxgGuWpgExRS', 20, 'up0000020@ac.upatras.gr', 'Μαρία', 'Βαβούρης', 'Δημήτρης', '6921234567', '2109412345', 'student'),
+('up0000021', '$2y$10$bWjU.92tu8o/IcRTLhojR.4az.p.ql/IeQF15TpBxxKEv146ptLOW', 21, 'up0000021@ac.upatras.gr', 'Παναγιώτης', 'Καραφωτιάς', 'Σταύρος', '6922234567', '2109512345', 'student'),
+('up0000022', '$2y$10$TayJa3ieAuFQjRn/zupCTOTJOCgFRthFUQOxB7IGBcfxc7obiCzCa', 22, 'up0000022@ac.upatras.gr', 'Γιάννης', 'Κωστόπουλος', 'Γιώργος', '6923234567', '2109612345', 'student'),
+('up0000023', '$2y$10$YIs38KF9CQ93EngpFb/Aye8ljqzDcS7yva0yMiQOYN0rigsQTcb4C', 23, 'up0000023@ac.upatras.gr', 'Φώτης', 'Κουτσογιάννης', 'Στέφανος', '6924234567', '2109712345', 'student'),
+('up0000024', '$2y$10$4/9et1yp2drXskRITU/ZmODsV6R4qU0a/rDnomwvdmP0ZmZZvTpZe', 24, 'up0000024@ac.upatras.gr', 'Νίκος', 'Πιλάτος', 'Αριστείδης', '6925234567', '2109812345', 'student'),
+('up0000025', '$2y$10$jLr3VkTb4Y1c/m0sRSmRo.r3G9f1XuM3LXhc6xmDkATg4NOgjpsUC', 25, 'up0000025@ac.upatras.gr', 'Χρήστος', 'Καλαφάτης', 'Στέφανος', '6926234567', '2109912345', 'student'),
+('up0000026', '$2y$10$NwOIrVYjWGBuuEFjDKjq.ex0cVs9zmBuuuyK64bLYbUYBujy6n2nS', 26, 'up0000026@ac.upatras.gr', 'Νίκος', 'Τσολάκης', 'Δημήτρης', '6927234567', '2109912345', 'student'),
+('up0000027', '$2y$10$X2eMrbJ6NQx9.Mvfs/IQtup0Z47gEfH7IxNCqLRlBYlT0ID3kJVN.', 27, 'up0000027@ac.upatras.gr', 'Δημήτρης', 'Φωτόπουλος', 'Αλέξανδρος', '6928234567', '2109512345', 'student'),
+('up0000028', '$2y$10$2n1.xlXcqkIMBdBSlWLCre8N2c1fH8NRG1I1QTFqEdLh2b53P0F9y', 28, 'up0000028@ac.upatras.gr', 'Κωνσταντίνος', 'Αντωνόπουλος', 'Γεώργιος', '6929234567', '2109612345', 'student'),
+('up0000029', '$2y$10$PIvJ3eJ/xwWw3hbiwyVm9eUcyFR1O8F73EcF8Jt9.CmZSm/J1V0WS', 29, 'up0000029@ac.upatras.gr', 'Γιώργος', 'Κοκκίνης', 'Νικόλαος', '6930234567', '2109712345', 'student'),
+('up0000030', '$2y$10$eeht7HTGFUPUwMg7il.n5u/WgWlHbBCYo6wwpEMJhPi9dTNRBPC.i', 30, 'up0000030@ac.upatras.gr', 'Αντώνης', 'Μητσούλης', 'Σπύρος', '6931234567', '2109812345', 'student');
 
 -- --------------------------------------------------------
 
@@ -220,13 +270,6 @@ CREATE TABLE `user_tokens` (
   `user` varchar(30) NOT NULL,
   `expiration_date` datetime NOT NULL DEFAULT (current_timestamp() + interval 1 hour)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `user_tokens`
---
-
-INSERT INTO `user_tokens` (`token`, `user`, `expiration_date`) VALUES
-('700f5ebde0549a09d7b27c6acb7aa3012b2e36f39abbe488a2590a7640bf316d', 'up0000001', '2024-11-23 00:46:53');
 
 --
 -- Indexes for dumped tables
@@ -316,10 +359,28 @@ ALTER TABLE `diplomatiki`
 --
 
 --
+-- Constraints for table `address`
+--
+ALTER TABLE `address`
+  ADD CONSTRAINT `fk_address_users` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `diplomatiki`
 --
 ALTER TABLE `diplomatiki`
   ADD CONSTRAINT `fk_professor_diplomatiki` FOREIGN KEY (`professor`) REFERENCES `professor` (`username`);
+
+--
+-- Constraints for table `professor`
+--
+ALTER TABLE `professor`
+  ADD CONSTRAINT `fk_professor_users` FOREIGN KEY (`username`) REFERENCES `users` (`username`);
+
+--
+-- Constraints for table `student`
+--
+ALTER TABLE `student`
+  ADD CONSTRAINT `fk_student_users` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 DELIMITER $$
 --
