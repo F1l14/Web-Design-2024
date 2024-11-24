0ab8864a138069991be51990092710aeb052a91b
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2024-11-24 21:40:27 +0200

    prefills edit inputs with data from the db

---
+++ php/loadSingleThesis.php
@@ -0,0 +1,37 @@
+<?php
+include_once "dbconn.php";
+$reply = new stdClass;
+$reply->message = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_COOKIE["user"])) {
+        $user = json_decode($_COOKIE['user']);
+        $json = file_get_contents("php://input");
+        $decoded =json_decode($json);
+        $thesisId = $decoded->id;
+
+        try {
+            $stmt = $conn->prepare(
+                "SELECT * FROM diplomatiki WHERE professor = ? AND id = ?"
+            );
+            $stmt->bind_param("si", $user->username, $thesisId);
+            $stmt->execute();
+            $result = $stmt->get_result();
+        } catch (mysqli) {
+            $reply->message = "sqlError";
+            echo json_encode($reply);
+        }
+
+        // Fetch all rows into an array
+       
+        if ($result->num_rows > 0) {
+           
+            $reply->data = $result->fetch_assoc();
+            $reply->message = "ok";
+            echo json_encode($reply);
+        } else {
+            $reply->message = "empty";
+            echo json_encode($reply);
+        }
+    }
+}
