05df43f0cb75d2decbb81086519d44cd28878be8
F1l14 <filipmn42@gmail.com>
2024-11-24 15:01:51 +0200

    transfered search to separate file

--- php/professor/createThesis.php
+++ php/professor/createThesis.php
@@ -106,30 +106,4 @@ updateActivity();
 
 <script src="/Web-Design-2024/js/professor/insertThesis.js" defer></script>
 <script type="module" src="/Web-Design-2024/js/professor/createThesisHandler.js" defer></script>
-
-
-
-<script>
-    const search = document.getElementById("searchThesis");
-    const table = document.getElementById("thesisTable");
-    var rows = document.querySelectorAll("#thesisTable tr");
-    window.addEventListener("tableLoaded", function(){
-        rows = document.querySelectorAll("#thesisTable tr");
-        console.log(rows);
-    }) ;                        
-    search.addEventListener("keyup", function(){
-        
-        console.log(rows);
-        let value = search.value.toLowerCase();
-            
-            rows.forEach(function(row){
-                console.log(row.textContent.toLowerCase());
-                if(row.textContent.toLowerCase().indexOf(value) > -1){
-                    row.style.display = "";
-                }else{
-                    row.style.display = "none";
-                }
-                
-            });
-    });
-</script>
+<script src="/Web-Design-2024/js/professor/searchThesis.js"></script>
