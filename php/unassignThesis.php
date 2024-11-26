ceff2e014bea1c4b5439b4dba980dd357310c899
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2024-11-26 18:28:47 +0200

    now changes the status of the student that the thesis was assigned
    Co-authored-by: Filippos Minopetros <100228368+F1l14@users.noreply.github.com>

--- php/unassignThesis.php
+++ php/unassignThesis.php
@@ -9,6 +9,45 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $id = $data['id'];
         $user = json_decode($_COOKIE["user"]);
         $resp = new stdClass();
+        
+        try {
+            $stmt = $conn->prepare(
+                "SELECT student
+                        FROM diplomatiki 
+                        WHERE id = ? AND professor = ?;"
+            );
+            $stmt->bind_param("is", $id, $user->username);
+            $stmt->execute();
+
+            $studentUsername = $stmt->get_result()->fetch_assoc()['student'];
+            $resp->state = "valid";
+            $resp->data = $id ." ". $user->username;
+            // $resp->state = $title . $description . $id . $user->username;
+        } catch (Exception $e) {
+            $resp->state = "SQL Error: on Thesis Update";
+            $resp->error = $e->getMessage(); // Log the specific error message
+            echo json_encode($resp);
+        }
+        
+
+        try {
+            $stmt = $conn->prepare(
+                "UPDATE student
+                        SET status = 'available'
+                        WHERE username = ?;"
+            );
+            $stmt->bind_param("s", $studentUsername);
+            $stmt->execute();
+            $resp->state = "valid";
+            $resp->data = $id ." ". $studentUsername;
+            // $resp->state = $title . $description . $id . $user->username;
+        } catch (Exception $e) {
+            $resp->state = "SQL Error: on Thesis Update";
+            $resp->error = $e->getMessage(); // Log the specific error message
+            echo json_encode($resp);
+        }
+
+
         try {
             $stmt = $conn->prepare(
                 "UPDATE diplomatiki
