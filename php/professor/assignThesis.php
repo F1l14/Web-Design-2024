16c0c18e1b76101254da9933d0e7e3ccff41113c
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2024-11-26 16:20:53 +0200

    added body
    Co-authored-by: Filippos Minopetros <100228368+F1l14@users.noreply.github.com>

--- php/professor/assignThesis.php
+++ php/professor/assignThesis.php
@@ -3,3 +3,66 @@ require_once("../tokenFunctions.php");
 roleProtected("professor");
 updateActivity();
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>ΣΔΔΕ</title>
+    <link rel="stylesheet" href="/Web-Design-2024/css/styles.css">
+    <link rel="stylesheet" href="/Web-Design-2024/css/header.css">
+    <link rel="stylesheet" href="/Web-Design-2024/css/assignThesis.css">
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- icon -->
+    <link rel="icon" type="image/svg+xml" href="/Web-Design-2024/icons/websiteIcon.svg">
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/Web-Design-2024/header.html' ?>
+</head>
+
+<body>
+    <div id="mainContainer" class="container align-items-center justify-content-center">
+        <div id="innerContainer" class="container box">
+            <div id="unassignedThesis" class="container">
+                <h3>Προς ανάθεση</h3>
+                <hr>
+                <table id="unassignedTable" class="table">
+                    <tbody>
+                        
+                    </tbody>
+                </table>
+
+            </div>
+
+
+            <div id="assignedThesis" class="container">
+                <h3>Κατοχυρωμένα</h3>
+                <hr>
+                <table id="assignedTable" class="table">
+                    <tbody>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+
+        <!-- ======================================================================================== -->
+        <canvas class="background"></canvas>
+</body>
+
+<!-- bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+<!-- bacgkround lib -->
+<script src="/Web-Design-2024/particles.js-master/particles.js-master/dist/particles.min.js"></script>
+<script src="/Web-Design-2024/js/backgroundOptions.js"></script>
+
+
+<script src="/Web-Design-2024/js/professor/assignThesis/loadThesisAssignment.js" defer></script>
+</html>
