2b1d3023c112a6f55192e5e3c820309963348a65
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2024-11-26 16:20:17 +0200

    loads all thesis that have been assigned to students
    Co-authored-by: Filippos Minopetros <100228368+F1l14@users.noreply.github.com>

---
+++ php/loadAssignedThesis.php
@@ -0,0 +1,37 @@
+<?php
+include_once "dbconn.php";
+$reply = new stdClass;
+$reply->message = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_COOKIE["user"])) {
+        $user = json_decode($_COOKIE['user']);
+
+        try {
+            $stmt = $conn->prepare(
+                "SELECT title, id FROM diplomatiki WHERE professor = ? AND status = 'anathesi'"
+            );
+            $stmt->bind_param("s", $user->username);
+            $stmt->execute();
+            $result = $stmt->get_result();
+        } catch (mysqli) {
+            $reply->message = "sqlError";
+            echo json_encode($reply);
+        }
+
+        // Fetch all rows into an array
+        $data = array();
+        if ($result->num_rows > 0) {
+            // while ($row = $result->fetch_assoc()) {
+
+            // }
+            $data = $result->fetch_all(MYSQLI_ASSOC);
+            $reply->data = $data;
+            $reply->message = "ok";
+            echo json_encode($reply);
+        } else {
+            $reply->message = "empty";
+            echo json_encode($reply);
+        }
+    }
+}
