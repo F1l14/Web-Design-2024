28e2275e8fe6a29b1506ca703f43e17722cce62e
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2024-11-27 20:54:07 +0200

    format
    Co-authored-by: Filippos Minopetros <100228368+F1l14@users.noreply.github.com>

--- js/professor/searchThesis.js
+++ js/professor/searchThesis.js
@@ -1,22 +1,22 @@
 const search = document.getElementById("searchThesis");
-    const table = document.getElementById("thesisTable");
-    var rows = document.querySelectorAll("#thesisTable tr");
-    window.addEventListener("tableLoaded", function(){
-        rows = document.querySelectorAll("#thesisTable tr");
-        // console.log(rows);
-    }) ;                        
-    search.addEventListener("keyup", function(){
-        
-        console.log(rows);
-        let value = search.value.toLowerCase();
-            
-            rows.forEach(function(row){
-                // console.log(row.textContent.toLowerCase());
-                if(row.textContent.toLowerCase().indexOf(value) > -1){
-                    row.style.display = "";
-                }else{
-                    row.style.display = "none";
-                }
-                
-            });
+const table = document.getElementById("thesisTable");
+var rows = document.querySelectorAll("#thesisTable tr");
+window.addEventListener("tableLoaded", function () {
+    rows = document.querySelectorAll("#thesisTable tr");
+    // console.log(rows);
+});
+search.addEventListener("keyup", function () {
+
+    console.log(rows);
+    let value = search.value.toLowerCase();
+
+    rows.forEach(function (row) {
+        // console.log(row.textContent.toLowerCase());
+        if (row.textContent.toLowerCase().indexOf(value) > -1) {
+            row.style.display = "";
+        } else {
+            row.style.display = "none";
+        }
+
     });
+});
