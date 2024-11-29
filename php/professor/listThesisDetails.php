77001c86ea45519ddf6e2152c82a6e0900e6118e
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2024-11-30 00:57:53 +0200

    moved script to seperate file

--- php/professor/listThesisDetails.php
+++ php/professor/listThesisDetails.php
@@ -41,62 +41,6 @@ updateActivity();
 <script src="/Web-Design-2024/particles.js-master/particles.js-master/dist/particles.min.js"></script>
 <script src="/Web-Design-2024/js/backgroundOptions.js"></script>
 
-
-<!-- <script src="/Web-Design-2024/js/professor/listThesis/listThesis.js" defer></script>
-<script src="/Web-Design-2024/js/professor/listThesis/filterTable.js" defer></script>
-<script src="/Web-Design-2024/js/professor/searchThesis.js" defer></script>
-
-<script src="/Web-Design-2024/js/professor/listThesis/export.js"></script> -->
+<script src="/Web-Design-2024/js/professor/listThesis/details.js"></script>
 
 </html>
-
-<script>
-    // Get the current URL
-    const queryParams = new URLSearchParams(window.location.search);
-
-    // Retrieve the 'thesisId' parameter
-    const thesisId = queryParams.get('thesisId');
-
-    if (thesisId) {
-        console.log(`Thesis ID: ${thesisId}`);
-    } else {
-        console.log("No thesisId found in the URL.");
-    }
-
-    window.addEventListener("load", loadDetails)
-
-    async function loadDetails() {
-        fetch(`../thesisDetails.php?thesisId=${thesisId}`, {
-            method: "GET",
-            headers: { 'Accept': 'application/json' }
-        })
-            .then(response => {
-                return response.text().then(text => {
-                    console.log("Raw Response:", text);
-                    try {
-                        return JSON.parse(text); // Try parsing the JSON
-                    } catch (error) {
-                        console.error("JSON Parsing Error:", error);
-                        throw error; // Rethrow the error to be caught below
-                    }
-                });
-            })
-            .then(data => {
-                if (data.message != "empty") {
-                    console.log(data.data)
-                    // Object.entries(data.data).forEach(([key, value]) => {
-                    //     console.log(`${key} : ${value}`)
-                    // });
-                } else if (data.message == "sqlError") {
-                    console.log("sqlError on insert thesis table");
-                }
-
-            })
-
-            .catch(error => {
-                console.error("Error Occured:", error);
-            })
-            ;
-    }
-
-</script>
