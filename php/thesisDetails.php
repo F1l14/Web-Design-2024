9975f8768818fc240e0c5f194dca0b924b7e0217
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2024-11-29 18:48:32 +0200

    query script for details

---
+++ php/thesisDetails.php
@@ -0,0 +1,39 @@
+<?php
+include_once "dbconn.php";
+$reply = new stdClass;
+$reply->message = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    if (isset($_COOKIE["user"])) {
+        $user = json_decode($_COOKIE['user']);
+        $id = $_GET['id'];
+        try {
+            $stmt = $conn->prepare(
+                "SELECT * FROM diplomatiki 
+                INNER JOIN epitroph ON diplomatiki.id = epitroph.diplomatiki
+                INNER JOIN diplomatiki_log ON diplomatiki.id = diplomatiki_id.
+                WHERE id = ? AND ? IN (prof1, prof2, prof3) AND diplomatiki.status <> 'diathesimi';"
+            );
+            
+            $stmt->bind_param("is", $id,$user->username);
+            $stmt->execute();
+            $result = $stmt->get_result();
+        } catch (mysqli) {
+            $reply->message = $conn->error;
+            echo json_encode($reply);
+            return;
+        }
+
+        if ($result->num_rows > 0) {
+            $data = $result->fetch_assoc();
+            $reply->data = $data;
+            $reply->message = "ok";
+            $reply->username = $user->username;
+            echo json_encode($reply);
+        } else {
+            $reply->message = "empty";
+            echo json_encode($reply);
+        }
+    }
+}
+?>
