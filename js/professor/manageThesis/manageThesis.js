10a39a7841e9512ab8b88f371b6bd81be02565f8
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2024-12-01 20:25:43 +0200

    main body added
    Co-authored-by: Filippos Minopetros <100228368+F1l14@users.noreply.github.com>

---
+++ js/professor/manageThesis/manageThesis.js
@@ -0,0 +1,104 @@
+thesisTbody = document.getElementById("thesisTbody");
+
+window.addEventListener("load", loadThesis);
+// window.addEventListener("load", function () {
+//     insert("Titlos", "myrole", "energi", 4);
+//     insert("asdf", "myrole", "epeksergasia", 4);
+//     insert("fffffff", "myrole", "peratomeni", 4);
+//     insert("ggggggggggg", "myrole", "akiromeni", 4);
+//     insert("hhhhhhhhhhh", "myrole", "anathesi", 4);
+//     const loadedEvent = new CustomEvent("tableLoaded");
+//     window.dispatchEvent(loadedEvent);
+// });
+
+async function loadThesis() {
+    fetch("../manageThesisList.php", {
+        method: "POST",
+        //Accepting json response from backend
+        headers: { 'Accept': 'application/json' }
+    })
+
+        .then(response => {
+            return response.text().then(text => {
+                console.log("Raw Response:", text);
+                try {
+                    return JSON.parse(text); // Try parsing the JSON
+                } catch (error) {
+                    console.error("JSON Parsing Error:", error);
+                    throw error; // Rethrow the error to be caught below
+                }
+            });
+        })
+
+        .then(data => {
+            if (data.message != "empty") {
+                let role ="";
+                Object.entries(data.data).forEach(([key, value]) => {
+                    if(data.username===value.prof1){
+                        role ="Επιβλέπων";
+                    }else{
+                        role = "Επιτροπή";
+                    }
+                    
+                    insert(value.title, role, value.status, value.id);
+
+                });
+                const loadedEvent = new CustomEvent("tableLoaded");
+                window.dispatchEvent(loadedEvent);
+            } else if (data.message == "sqlError") {
+                console.log("sqlError on insert thesis table");
+            }
+
+        })
+
+        .catch(error => {
+            console.error("Error Occured:", error);
+        })
+        ;
+}
+
+
+function insert(title, role, state, id) {
+
+    const row = thesisTbody.insertRow();
+    row.id = id;
+
+    titleCell = row.insertCell(0);
+    titleCell.textContent = title;
+
+    const roleInput = document.createElement("input");
+    roleInput.value = role;
+    roleInput.readOnly = true;
+    roleInput.className = "disabledInput";
+    roleCell = row.insertCell(1);
+    roleCell.appendChild(roleInput);
+
+    const stateInput = document.createElement("input");
+    greekState ="";
+    switch (state) {
+        case "energi": { greekState ="Ενεργή";stateInput.style.outline = "2px solid #40eb34 "; break; }
+        case "eksetasi": { greekState ="Εξέταση";stateInput.style.outline = "2px solid #0a6bff "; break; }
+        case "anathesi": { greekState ="Ανάθεση";stateInput.style.outline = "2px solid #fcce00"; break; }
+    }
+    stateInput.value = greekState;
+    stateInput.readOnly = true;
+    stateInput.className = "disabledInput";
+    stateCell = row.insertCell(2);
+    stateCell.appendChild(stateInput);
+
+    const openIcon = document.createElement("img");
+    openIcon.src = "/Web-Design-2024/icons/edit_light.svg";
+
+    const openButton = document.createElement("button");
+    openButton.appendChild(openIcon);
+    openButton.className = "optionButton";
+    openButton.style.backgroundColor = "#868e94";
+    openButton.addEventListener("click",function(){
+        window.location.href = `manage/${state}.php?thesisId=${encodeURIComponent(id)}`;
+    });
+
+    moreCell = row.insertCell(3);
+    moreCell.className = "optionCell";
+    moreCell.appendChild(openButton);
+
+}
--- php/professor/manageThesis.php
+++ php/professor/manageThesis.php
@@ -3,3 +3,134 @@ require_once("../tokenFunctions.php");
 roleProtected("professor");
 updateActivity();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>ΣΔΔΕ</title>
+
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="/Web-Design-2024/css/styles.css">
+    <link rel="stylesheet" href="/Web-Design-2024/css/header.css">
+    <link rel="stylesheet" href="/Web-Design-2024/css/listThesis.css">
+    <!-- icon -->
+    <link rel="icon" type="image/svg+xml" href="/Web-Design-2024/icons/websiteIcon.svg">
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/Web-Design-2024/header.html' ?>
+</head>
+
+<body>
+    <div id="mainContainer" class="container align-items-center justify-content-center">
+        <div id="innerContainer" class="container box">
+            <div class="row g-0">
+                <div class="col-4 m-auto dropdown align-content-center">
+                    <button id="filter" type="button" class="btn btn-secondary dropdown-toggle"
+                        data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">Filter
+                        <img src="/Web-Design-2024/icons/tune.svg" />
+                    </button>
+
+
+                    <form class="dropdown-menu p-2 text-center">
+                        <div class="accordion accordion-flush" id="stateAccordion">
+                            <div class="accordion-item">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                                    <h6 class="accordion-header">Ρόλος</h6>
+                                </button>
+                            </div>
+
+                            <div id="collapseOne" class="accordion-collapse collapse" >
+                                <div class="dropdownTable">
+                                    <a href="#" id="epivlepon" name="Επιβλέπων" class="filterButton btn btn-primary" role="button"
+                                        data-bs-toggle="button">Επιβλέπων</a>
+                                    <a href="#" id="melos" name="Επιτροπή" class="filterButton btn btn-primary" role="button"
+                                        data-bs-toggle="button">Μέλος Τριμελούς</a>
+                                </div>
+                            </div>
+
+                            <div class="accordion-item">
+
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                    <h6 class="accordion-header">Κατάσταση</h6>
+                                </button>
+                            </div>
+
+                            <div id="collapseTwo" class="accordion-collapse collapse" >
+                                <div class="accordion-body">
+                                    <div class="dropdownTable">
+                                        <a href="#" id="anathesi" name ="Ανάθεση" class="filterButton btn btn-primary" role="button"
+                                            data-bs-toggle="button">Υπο Ανάθεση</a>
+                                        <a href="#" id="energi" name ="Ενεργή" class="filterButton btn btn-primary" role="button"
+                                            data-bs-toggle="button">Ενεργή</a>
+                                        <a href="#" id="peratomeni" name ="Περατωμένη" class="filterButton btn btn-primary" role="button"
+                                            data-bs-toggle="button">Περατωμένη</a>
+                                        <a href="#" id="akiromeni"name ="Ακυρωμένη"  class="filterButton btn btn-primary" role="button"
+                                            data-bs-toggle="button">Ακυρωμένη</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="row d-flex justify-content-center text-center">
+
+
+
+                        </div>
+                    </form>
+                </div>
+                <div class="col-1 m-3 align-content-center text-center">
+                    <button id="download-json" class="btn btn-secondary"><img
+                            src="/Web-Design-2024/icons/download.svg">JSON</button>
+                </div>
+                <div class="col-1 m-3 align-content-center text-center">
+                    <button id="download-csv" class="btn btn-secondary">
+                        <img src="/Web-Design-2024/icons/download.svg"> CSV</button>
+                </div>
+
+
+                <div class="col-4 m-3 align-content-center">
+                    <input class="form-control" id="searchThesis" type="text" placeholder="Search...">
+                </div>
+            </div>
+            <div class="row g-0">
+                <table id="thesisTable" class="table">
+                    <thead>
+                        <tr>
+                            <th id="title" scope="col" class="text-secondary">Τίτλος</th>
+                            <th id="role" scope="col" class="text-secondary">Ρόλος</th>
+                            <th scope="state" class="text-secondary">Κατάσταση</th>
+                            <th scope="more" class="text-secondary"></th>
+                        </tr>
+                    </thead>
+
+                    <tbody id="thesisTbody">
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <!-- ======================================================================================== -->
+        <canvas class="background"></canvas>
+    </div>
+</body>
+
+<!-- bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+<!-- bacgkround lib -->
+<script src="/Web-Design-2024/particles.js-master/particles.js-master/dist/particles.min.js"></script>
+<script src="/Web-Design-2024/js/backgroundOptions.js"></script>
+
+
+<script src="/Web-Design-2024/js/professor/manageThesis/manageThesis.js" defer></script>
+<script src="/Web-Design-2024/js/professor/listThesis/filterTable.js" defer></script>
+<script src="/Web-Design-2024/js/professor/searchThesis.js" defer></script>
+
+<script src="/Web-Design-2024/js/professor/listThesis/export.js"></script>
+
+</html>
