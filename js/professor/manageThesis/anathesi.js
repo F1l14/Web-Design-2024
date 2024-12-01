186fde2512de30cc9e66c821c706c5796f4b5c78
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2024-12-01 22:30:31 +0200

    backend cooperation with frontend
    Co-authored-by: Filippos Minopetros <100228368+F1l14@users.noreply.github.com>

---
+++ js/professor/manageThesis/anathesi.js
@@ -0,0 +1,129 @@
+// Get the current URL
+const queryParams = new URLSearchParams(window.location.search);
+
+// Retrieve the 'thesisId' parameter
+const thesisId = queryParams.get('thesisId');
+const cancelButton = document.getElementById("cancelThesis");
+window.addEventListener("load", loadDetails)
+
+async function loadDetails() {
+    fetch(`../../anathesiDetails.php?thesisId=${thesisId}`, {
+        method: "GET",
+        headers: { 'Accept': 'application/json' }
+    })
+        .then(response => {
+            return response.text().then(text => {
+                console.log("Raw Response:", text);
+                try {
+                    return JSON.parse(text); // Try parsing the JSON
+                } catch (error) {
+                    console.error("JSON Parsing Error:", error);
+                    throw error; // Rethrow the error to be caught below
+                }
+            });
+        })
+        .then(data => {
+            if (data.state != "SQL Error") {
+
+                title = data.data[0]["title"];
+                const titleInput = document.getElementById("titleInput");
+                titleInput.value = title;
+
+                student = `${data.data[0]["student"]} | ${data.data[0]["firstname"]} ${data.data[0]["lastname"]}`;
+                const studentInput = document.getElementById("studentInput");
+                studentInput.value = student;
+
+
+                const prof1 = data.data[0]["prof1"];
+                const prof1Input = document.getElementById("prof1");
+                prof1Input.value = prof1;
+                if (data.prof_names.firstname_prof1 !== null) new bootstrap.Tooltip(prof1Input, { title: `${data.prof_names.firstname_prof1} ${data.prof_names.lastname_prof1}` });
+
+
+                const prof2 = data.data[0]["prof2"];
+                const prof2Input = document.getElementById("prof2");
+                prof2Input.value = prof2;
+                if (data.prof_names.firstname_prof2 !== null) new bootstrap.Tooltip(prof2Input, { title: `${data.prof_names.firstname_prof2} ${data.prof_names.lastname_prof2}` });
+
+                const prof3 = data.data[0]["prof3"];
+                const prof3Input = document.getElementById("prof3");
+                prof3Input.value = prof3;
+                if (data.prof_names.firstname_prof3 !== null) new bootstrap.Tooltip(prof3Input, { title: `${data.prof_names.firstname_prof3} ${data.prof_names.lastname_prof3}` });
+
+
+                if (!data.epivlepon) {
+                    cancelButton.disabled = true;
+                } else {
+                    cancelButton.addEventListener("click", function () {
+                        cancelThesis(thesisId);
+
+                    })
+                }
+
+                const logTable = document.getElementById("logTable");
+                if (data.log !== undefined) {
+                    data.log.forEach((entry) => {
+                        const row = logTable.insertRow();
+                        date = row.insertCell(0);
+                        date.textContent = entry["date"];
+                        katastasi = row.insertCell(1);
+                        katastasi.textContent = entry["new_state"];
+
+                        professor = row.insertCell(2);
+                        professor.textContent = entry["invited_professor"];
+                    });
+                }
+
+            } else if (data.message == "SQL Error") {
+                console.log(data.message);
+            }
+
+        })
+
+        .catch(error => {
+            console.error("Error Occured:", error);
+        })
+        ;
+}
+
+
+
+async function cancelThesis(id) {
+    const rowId = {
+        id: id
+    };
+    const userConfirmation = confirm("Αναίρεση ανάθεσης?");
+    if (userConfirmation) {
+        fetch("../../unassignThesis.php", {
+            method: "POST",
+            body: JSON.stringify(rowId),
+            //Accepting json response from backend
+            headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' }
+        })
+
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status} ${response.statusText}`);
+                }
+                return response.json();
+            })
+
+            .then(data => {
+                // console.log(data.loginError);
+                // console.log("js: "+data.response);
+                switch (data.state) {
+                    case "missing": { alert("Does not Exist"); console.log(data.error); window.href = history.back(); break; }
+                    case "valid": {
+                        window.location.href = '/Web-Design-2024/php/professor/manageThesis.php';
+                        break;
+                    }
+                    default: { console.log(data.error); break; }
+                }
+            })
+
+            .catch(error => {
+                console.error("Error Occured:", error);
+            })
+
+    }
+}
---
+++ php/anathesiDetails.php
@@ -0,0 +1,121 @@
+<?php
+include_once "dbconn.php";
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$reply = new stdClass;
+$reply->message = "";
+
+if (isset($_GET['thesisId']) && isset($_COOKIE["user"])) {
+    $user = json_decode($_COOKIE['user']);
+    $id = intval($_GET['thesisId']);
+    try {
+        $stmt = $conn->prepare(
+            "SELECT title, student, firstname, lastname, prof1, prof2, prof3 FROM diplomatiki 
+                        INNER JOIN epitroph ON diplomatiki.id = epitroph.diplomatiki
+                        INNER JOIN student ON diplomatiki.student = student.username
+                        INNER JOIN users ON student.username = users.username
+                        WHERE diplomatiki.id = ? AND ? IN (prof1, prof2, prof3) AND diplomatiki.status = 'anathesi';"
+        );
+
+        $stmt->bind_param("is", $id, $user->username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+            $data = $result->fetch_all(MYSQLI_ASSOC);
+            
+            if($data[0]['prof1'] == $user->username) {
+                $reply->epivlepon = true;
+            }else{
+                $reply->epivlepon = false;
+            }
+
+            $reply->data = $data;
+            $reply->message = "ok";
+            $reply->username = $user->username;
+            // echo json_encode($reply);
+        } else {
+            $reply->message = "empty";
+            echo json_encode($reply);
+        }
+    } catch (mysqli) {
+        $reply->message = $conn->error;
+        echo json_encode($reply);
+        return;
+    }
+
+    try {
+        $stmt = $conn->prepare(
+            "SELECT epitroph_app_log.date, epitroph_app_log.new_state, epitroph_app_log.invited_professor FROM diplomatiki 
+                        INNER JOIN epitroph ON diplomatiki.id = epitroph.diplomatiki
+                        INNER JOIN epitroph_app_log ON diplomatiki.id = epitroph_app_log.diplomatiki
+                        WHERE diplomatiki.id = ? AND ? IN (prof1, prof2, prof3) AND diplomatiki.status = 'anathesi'
+                        ORDER BY epitroph_app_log.date DESC;"
+        );
+
+        $stmt->bind_param("is", $id, $user->username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+            $data = $result->fetch_all(MYSQLI_ASSOC);
+            
+            $reply->log = $data;
+            $reply->message = "ok";
+            $reply->username = $user->username;
+            // echo json_encode($reply);
+        }
+    } catch (mysqli) {
+        $reply->message = $conn->error;
+        echo json_encode($reply);
+        return;
+    }
+
+    try {
+        $stmt = $conn->prepare(
+            "SELECT 
+                        -- Fields for the first foreign key (prof1)
+                        c1.firstname AS firstname_prof1,
+                        c1.lastname AS lastname_prof1,
+
+                        -- Fields for the second foreign key (prof2)
+                        c2.firstname AS firstname_prof2,
+                        c2.lastname AS lastname_prof2,
+
+                        -- Fields for the third foreign key (prof3)
+                        c3.firstname AS firstname_prof3,
+                        c3.lastname AS lastname_prof3
+                    FROM 
+                        epitroph a
+                    
+                    INNER JOIN diplomatiki ON diplomatiki.id = a.diplomatiki
+                    
+                    LEFT JOIN professor b1 ON a.prof1 = b1.username
+                    LEFT JOIN users c1 ON b1.username = c1.username
+
+                    LEFT JOIN professor b2 ON a.prof2 = b2.username
+                    LEFT JOIN users c2 ON b2.username = c2.username
+
+                    LEFT JOIN professor b3 ON a.prof3 = b3.username
+                    LEFT JOIN users c3 ON b3.username = c3.username
+                    
+                    WHERE diplomatiki.id = ? AND ? IN (prof1, prof2, prof3) AND diplomatiki.status = 'anathesi';
+"               
+        );
+
+        $stmt->bind_param("is", $id, $user->username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+            $prof_names = $result->fetch_assoc();
+            $reply->prof_names = $prof_names;
+            $reply->message = "ok";
+            $reply->username = $user->username;
+            echo json_encode($reply);
+        } else {
+            $reply->message = $stmt->affected_rows;
+            echo json_encode($reply);
+        }
+    } catch (mysqli) {
+        $reply->message = $conn->error;
+        echo json_encode($reply);
+        return;
+    }
+}
