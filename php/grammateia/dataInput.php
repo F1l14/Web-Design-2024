fae9496284f83dd0c45eeda41987241925679fbf
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2024-12-05 17:13:47 +0200

    data input unfinished
    Co-authored-by: Filippos Minopetros <100228368+F1l14@users.noreply.github.com>

--- php/grammateia/dataInput.php
+++ php/grammateia/dataInput.php
@@ -3,3 +3,62 @@ require_once("../tokenFunctions.php");
 roleProtected("grammateia");
 updateActivity();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>ΣΔΔΕ</title>
+
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="/Web-Design-2024/css/styles.css">
+    <link rel="stylesheet" href="/Web-Design-2024/css/header.css">
+    <link rel="stylesheet" href="/Web-Design-2024/css/listThesis.css">
+    <!-- icon -->
+    <link rel="icon" type="image/svg+xml" href="/Web-Design-2024/icons/websiteIcon.svg">
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/Web-Design-2024/header.html' ?>
+</head>
+
+<body>
+    <div id="mainContainer" class="container align-items-center justify-content-center">
+        <div id="innerContainer" class="container box" container-fluid>
+
+            <div class="row ">
+                <div class="col align-self-center">
+                    <form action="" enctype="multipart/form-data">
+                        <h3>Επισύναψη αρχείου Χρηστών</h3>
+                        <div class="input-group">
+                            <input class="form-control " name="fileToUpload" id="fileToUpload" type="file"
+                                accept=".json">
+                            <button class="btn btn-outline-secondary" type="button" id="removeFile">
+                                <img src="/Web-Design-2024/icons/x.svg" />
+                            </button>
+                        </div>
+                        <div class="centeredDiv">
+                            <button class="pageButton">upload</button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+        <!-- ======================================================================================== -->
+        <canvas class="background"></canvas>
+    </div>
+</body>
+
+<!-- bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+<!-- bacgkround lib -->
+<script src="/Web-Design-2024/particles.js-master/particles.js-master/dist/particles.min.js"></script>
+<script src="/Web-Design-2024/js/backgroundOptions.js"></script>
+
+</html>
