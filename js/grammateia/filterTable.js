5e482ce3c7a5b870fea97b8af53b26b06f7b86b1
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2024-12-09 22:26:59 +0200

    renamed currentRole to current and fixed if logic
    Co-authored-by: Filippos Minopetros <100228368+F1l14@users.noreply.github.com>

--- js/grammateia/filterTable.js
+++ js/grammateia/filterTable.js
@@ -13,9 +13,7 @@ function filterTable(currentButton, table, option) {
     let visibility = currentButton.getAttribute("aria-pressed");
     let atrVal = ""
     let cell = 0;
-    if (visibility === "false") {
-
-    } else {
+    if (visibility !== "false") {
         atrVal = "none";
     }
 
@@ -27,8 +25,8 @@ function filterTable(currentButton, table, option) {
     cell=1
 
     tableRows.forEach(function (row) {
-        currentRole = row.cells[cell].querySelector("input").value;
-        if (currentRole != currentButton.name) {
+        current = row.cells[cell].querySelector("input").value;
+        if (current != currentButton.name) {
             row.style.display = atrVal;
         }
     })
