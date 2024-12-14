858133c5077f6330f143a1a306ef7faf847d0cba
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2024-12-14 20:06:06 +0200

    return available professors
    Co-authored-by: Filippos Minopetros <100228368+F1l14@users.noreply.github.com>

---
+++ php/student/scripts/manage/anathesi/getAvailableProfs.php
@@ -0,0 +1,31 @@
+<?php
+include_once $_SERVER["DOCUMENT_ROOT"] . "/Web-Design-2024/php/dbconn.php";
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$resp = new stdClass;
+$resp->answer = false;
+if (isset($_COOKIE["user"])) {
+    try {
+        $stmt = $conn->prepare(
+            "SELECT firstname, lastname, username
+                    FROM professor
+                    INNER JOIN  users ON users.username = professor.username
+                    WHERE professor.status = available;"
+        );
+
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+            $resp->data = $result->fetch_assoc();
+            $resp->answer = true;
+            echo json_encode($resp);
+        } else {
+            echo json_encode($resp);
+            return;
+        }
+    } catch (mysqli_sql_exception) {
+
+        $resp->error = $conn->error; // Log the specific error message
+        echo json_encode($resp);
+        return;
+    }
+}
