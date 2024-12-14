47055e0d8c679300df54d42ef73fefed43b214af
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2024-12-14 19:49:21 +0200

    loads invitations for a thesis of  a student
    Co-authored-by: Filippos Minopetros <100228368+F1l14@users.noreply.github.com>

---
+++ php/student/scripts/manage/anathesi/getInvitations.php
@@ -0,0 +1,32 @@
+<?php
+include_once $_SERVER["DOCUMENT_ROOT"] . "/Web-Design-2024/php/dbconn.php";
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$resp = new stdClass;
+$resp->answer = false;
+if (isset($_COOKIE["user"])) {
+    $username = json_decode($_COOKIE['user'])->username;
+    try {
+        $stmt = $conn->prepare(
+            "SELECT invited_professor
+                    FROM epitroph_app
+                    INNER JOIN  diplomatiki ON diplomatiki.id = epitroph_app.diplomatiki
+                    WHERE diplomatiki.student = ?;"
+        );
+
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+            $resp->data = $result->fetch_assoc();
+            $resp->answer = true;
+        } else {
+            echo json_encode($resp);
+            return;
+        }
+    } catch (mysqli_sql_exception) {
+
+        $resp->error = $conn->error; // Log the specific error message
+        echo json_encode($resp);
+        return;
+    }
+}
