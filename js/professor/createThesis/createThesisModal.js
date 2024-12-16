a3cf087d1615b5f8c2a30705262a90a5a61f3234
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2024-12-16 22:00:07 +0200

    format
    Co-authored-by: Filippos Minopetros <100228368+F1l14@users.noreply.github.com>

--- js/professor/createThesis/createThesisModal.js
+++ js/professor/createThesis/createThesisModal.js
@@ -5,16 +5,16 @@ const title = document.getElementById("title");
 const description = document.getElementById("description")
 const removeFile = document.getElementById("removeFile");
 const file = document.getElementById("formFileSm");
-file.addEventListener("input", function(){
-    if(file.files.length===1){
+file.addEventListener("input", function () {
+    if (file.files.length === 1) {
         removeFile.style.display = "block";
-    }else{
+    } else {
         removeFile.style.display = "none";
     }
 });
 
-removeFile.addEventListener("click", function(){
-    file.value="";
+removeFile.addEventListener("click", function () {
+    file.value = "";
     removeFile.style.display = "none";
 })
 
@@ -29,10 +29,10 @@ const Modal = new bootstrap.Modal(modal);
 
 
 
-function resetModal(){
+function resetModal() {
     createForm.reset();
     title.style.border = "";
-    description.style.border= "";
+    description.style.border = "";
     file.style.border = "";
 }
 
@@ -40,61 +40,61 @@ function resetModal(){
 
 
 
-async function save(event){
+async function save(event) {
     // prevent default "submit redirection"
     event.preventDefault();
     // $=end of a string
     // i= case sensitive
     let filePattern = /\.(pdf|doc|docx|odt)$/i;
-    if(requiredText(title) && requiredText(description) && requiredFile(file)){
+    if (requiredText(title) && requiredText(description) && requiredFile(file)) {
         //=======pass to ajax here=============
         // filesize in bytes converting to mb with 2 rounding points
-        if((file.files[0].size/1024/1024).toFixed(2)>4) {
+        if ((file.files[0].size / 1024 / 1024).toFixed(2) > 4) {
             alert("Αρχείο μεγαλύτερο από: 4MB");
             file.value = "";
             return;
         }
-        if(!filePattern.exec(file.files[0].name)){
+        if (!filePattern.exec(file.files[0].name)) {
             alert("Λάθος τύπος αρχείου: .pdf, .doc, .docx, .odt");
             file.value = "";
             return;
         }
-            uploadThesis(event);
-            
+        uploadThesis(event);
 
-            Modal.hide();
-            resetModal();
-        
+
+        Modal.hide();
+        resetModal();
+
     }
-    else if (requiredText(title) && requiredText(description)){
+    else if (requiredText(title) && requiredText(description)) {
         uploadThesis(event);
         Modal.hide();
         resetModal();
     }
 }
 
-function requiredText(element){
+function requiredText(element) {
     // trim removes leading or trailing whitespaces
-    if(!element.value.trim()){
+    if (!element.value.trim()) {
         element.style.border = "3px solid red";
         return false;
-    }else{
+    } else {
         element.style.border = "3px solid #90ff6e";
         return true;
     }
 }
 
-function requiredFile(element){
-    if(file.files.length==0){
+function requiredFile(element) {
+    if (file.files.length == 0) {
         element.style.border = "3px solid red";
         return false;
-    }else{
+    } else {
         element.style.border = "3px solid #90ff6e";
         return true;
     }
 }
 
-async function uploadThesis(event){
+async function uploadThesis(event) {
     // event.preventDefault();
     var data = new FormData(event.target);
 
@@ -104,32 +104,31 @@ async function uploadThesis(event){
         method: "POST",
         body: data,
         //Accepting json response from backend
-        headers: {'Accept': 'application/json'}
+        headers: { 'Accept': 'application/json' }
     })
 
-    .then(response => 
-    {
-        
-        return response.text().then(text => {
-            //  console.log("Raw Response:", text);
-            try {
-                return JSON.parse(text); // Try parsing the JSON
-            } catch (error) {
-                console.error("JSON Parsing Error:", error);
-                throw error; // Rethrow the error to be caught below
-            }
-        });
-    })
+        .then(response => {
 
-    .then(data => {
+            return response.text().then(text => {
+                //  console.log("Raw Response:", text);
+                try {
+                    return JSON.parse(text); // Try parsing the JSON
+                } catch (error) {
+                    console.error("JSON Parsing Error:", error);
+                    throw error; // Rethrow the error to be caught below
+                }
+            });
+        })
 
-        deleteAllThesis();
-        loadThesis();
-    
-    })
+        .then(data => {
 
-    .catch(error => {
-        console.error("Error Occured:", error);
-    })
-    ;
+            deleteAllThesis();
+            loadThesis();
+
+        })
+
+        .catch(error => {
+            console.error("Error Occured:", error);
+        })
+        ;
 }
