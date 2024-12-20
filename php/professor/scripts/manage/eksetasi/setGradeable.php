893942edf03fe47dac20e937e6de95b962612374
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2024-12-20 19:44:28 +0200

    set thesis gradeable in db
    Co-authored-by: Filippos Minopetros <100228368+F1l14@users.noreply.github.com>

---
+++ php/professor/scripts/manage/eksetasi/setGradeable.php
@@ -0,0 +1,29 @@
+<?php
+include_once $_SERVER["DOCUMENT_ROOT"] . "/Web-Design-2024/php/dbconn.php";
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$resp = new stdClass;
+$resp->answer = false;
+$diplomatiki = $_GET['thesisId'];
+if (isset($_COOKIE["user"])) {
+    $username = json_decode($_COOKIE['user'])->username;
+
+    try {
+        $stmt = $conn->prepare(
+            "UPDATE diplomatiki
+                    SET gradeable=1
+                    WHERE id = ?;"
+        );
+
+        $stmt->bind_param("i", $diplomatiki);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($conn->affected_rows > 0) {
+            $resp->answer = true;
+        }
+    } catch (mysqli_sql_exception) {
+
+        $resp->error = $conn->error; // Log the specific error message
+        echo json_encode($resp);
+        return;
+    }
+}
