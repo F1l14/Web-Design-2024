c69e20712df4dcc8b7b379fb914dc4263bfdc1a1
F1l14 <filipmn42@gmail.com>
2024-12-22 19:48:01 +0200

    update

--- js/professor/stats/stats.js
+++ js/professor/stats/stats.js
@@ -4,6 +4,7 @@ const year = [2020, 2021, 2022, 2023, 2024];
 const values1 = [7.1, 8 , 3 ,4, 5];
 const values2 = [8, 8 , 3 ,4, 5];
 (async function(){
+    getXronosData();
     const xronos = document.getElementById('xronosCanvas');
     new window.Chart(xronos, {
         type: 'bar',
@@ -84,3 +85,37 @@ const values2 = [8, 8 , 3 ,4, 5];
         }
     });
 })();
+
+async function getXronosData(){
+    fetch(`scripts/stats/getXronos.php`, {
+        method: "GET",
+        headers: { 'Accept': 'application/json' }
+    })
+        .then(response => {
+            return response.text().then(text => {
+                // console.log("Raw Response:", text);
+                try {
+                    return JSON.parse(text); // Try parsing the JSON
+                } catch (error) {
+                    console.error("JSON Parsing Error:", error);
+                    throw error; // Rethrow the error to be caught below
+                }
+            });
+        })
+        .then(data => {
+            if (data.answer) {
+               console.log(data.data);
+            }
+        })
+
+        .catch(error => {
+            console.error("Error Occured:", error);
+        })
+        ;
+}
+async function getVathmosData(){
+
+}
+async function getPlithosData(){
+
+}
