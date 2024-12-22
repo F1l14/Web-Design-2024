46814af1ac203b1bb09501ec6a22c7b71b473aa0
F1l14 <filipmn42@gmail.com>
2024-12-22 21:09:30 +0200

    index update

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
   <!-- icon -->
   <link rel="icon" type="image/svg+xml" href="icons/websiteIcon.svg ">
 
@@ -28,162 +29,170 @@
 
 
   <div id="mainContainer" class="container align-content-center">
-    <div class="row  justify-content-between ">
+    <div class="row  justify-content-between">
 
-      <div id="calendar" class="col-lg-7 box  ">
+      <div class="col-lg-5 col-md-12 col-sm-12 order-lg-2 order-md-1 order-sm-1  align-content-center">
+        <div id="loginDiv">
+          <form class="darkbox" id="loginForm"
+            action="<?php echo htmlspecialchars('https://' . $_SERVER['HTTP_HOST'] . '/Web-Design-2024/php/index/login.php'); ?>"
+            method="POST">
 
-        <div class="row justify-content-center">
-          <div class="col-lg-3 col-sm-6">
-            <input id="startDate" class="form-control" type="date" />
-            <span id="startDateSelected"></span>
-          </div>
-          <img id="toDate" src="/Web-Design-2024/icons/assignment.svg"/>
-          <div class="col-lg-3 col-sm-7">
-            <input id="endDate" class="form-control dateInput" type="date" />
-            <span id="endDateSelected"></span>
-          </div>
+
+            <img id="accountCircle" src="icons/accountCircle.svg" />
+            <!-- <label for="username" class="loginText">Username<br></label> -->
+            <input id="userName" class="darkInput loginText" type="text" name="username" placeholder="Username"
+              maxlength="50">
+            <br>
+
+
+            <!-- <label for="pass" class="loginText">Password<br></label> -->
+            <input id="passWord" class="darkInput loginText" type="password" name="password" placeholder="Password"
+              maxlength="50">
+            <p id="loginError"></p>
+            <button type="submit" name="login">Login</button>
+
+
+          </form>
         </div>
-        <table id="diplomatikiTable" class="table table-striped ">
-          <thead>
-            <tr>
-              <th scope="col">Date</th>
-              <th scope="col">Student</th>
-              <th scope="col">Title</th>
-            </tr>
-          </thead>
 
-          <tbody>
-            <tr>
-              <th scope="row">30/12/2024</th>
-              <td>Μαρία Κουτσάκη</td>
-              <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
-                με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
-                Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
-            </tr>
-            <tr>
-              <th scope="row">30/12/2024</th>
-              <td>Μαρία Κουτσάκη</td>
-              <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
-                με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
-                Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
-            </tr>
-            <tr>
-              <th scope="row">30/12/2024</th>
-              <td>Μαρία Κουτσάκη</td>
-              <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
-                με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
-                Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
-            </tr>
-            <tr>
-              <th scope="row">30/12/2024</th>
-              <td>Μαρία Κουτσάκη</td>
-              <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
-                με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
-                Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
-            </tr>
-            <tr>
-              <th scope="row">30/12/2024</th>
-              <td>Μαρία Κουτσάκη</td>
-              <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
-                με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
-                Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
-            </tr>
-            <tr>
-              <th scope="row">30/12/2024</th>
-              <td>Μαρία Κουτσάκη</td>
-              <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
-                με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
-                Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
-            </tr>
-            <tr>
-              <th scope="row">30/12/2024</th>
-              <td>Μαρία Κουτσάκη</td>
-              <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
-                με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
-                Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
-            </tr>
-            <tr>
-              <th scope="row">30/12/2024</th>
-              <td>Μαρία κουτσομητροπουλος</td>
-              <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
-                με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
-                Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
-            </tr>
-            <tr>
-              <th scope="row">30/12/2024</th>
-              <td>Μαρία Κουτσάκη</td>
-              <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
-                με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
-                Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
-            </tr>
-            <tr>
-              <th scope="row">30/12/2024</th>
-              <td>Μαρία Κουτσάκη</td>
-              <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
-                με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
-                Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
-            </tr>
-            <tr>
-              <th scope="row">30/12/2024</th>
-              <td>Μαρία Κουτσάκη</td>
-              <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
-                με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
-                Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
-            </tr>
-            <tr>
-              <th scope="row">30/12/2024</th>
-              <td>Μαρία Κουτσάκη</td>
-              <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
-                με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
-                Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
-            </tr>
-            <tr>
-              <th scope="row">30/12/2024</th>
-              <td>Μαρία Κουτσάκη</td>
-              <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
-                με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
-                Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
-            </tr>
-            <tr>
-              <th scope="row">30/12/2024</th>
-              <td>Μαρία Κουτσάκη</td>
-              <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
-                με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
-                Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
-            </tr>
-            <tr>
-              <th scope="row">30/12/2024</th>
-              <td>Μαρία Κουτσάκη</td>
-              <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
-                με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
-                Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
-            </tr>
-          </tbody>
-        </table>
       </div>
 
-      <div class="col-lg-4  align-content-center">
-        <form class="darkbox" id="loginForm"
-          action="<?php echo htmlspecialchars('https://' . $_SERVER['HTTP_HOST'] . '/Web-Design-2024/php/index/login.php'); ?>"
-          method="POST">
+
+      <div id="calendar" class="col-lg-7 col-md-12 col-sm-12 order-lg-1  order-md-1 order-sm-2  box container">
+        <div class="d-flex">
+          <div id="calendarDateGroup"class="input-group mb-3">
+            <input id="calendarDate" type="date" class="form-control">
+            <span class="input-group-text" >
+              <img src="/Web-Design-2024/icons/calendar.svg" alt="">
+            </span>
+          </div>
 
 
-          <img id="accountCircle" src="icons/accountCircle.svg" />
-          <!-- <label for="username" class="loginText">Username<br></label> -->
-          <input id="userName" class="darkInput loginText" type="text" name="username" placeholder="Username"
-            maxlength="50">
-          <br>
 
+        </div>
+        <div id="calendarDiv" class=" justify-content-center ">
+          <table id="diplomatikiTable" class="table table-striped ">
+            <thead>
+              <tr>
+                <th scope="col">Date</th>
+                <th scope="col">Student</th>
+                <th scope="col">Title</th>
+              </tr>
+            </thead>
 
-          <!-- <label for="pass" class="loginText">Password<br></label> -->
-          <input id="passWord" class="darkInput loginText" type="password" name="password" placeholder="Password"
-            maxlength="50">
-          <p id="loginError"></p>
-          <button type="submit" name="login">Login</button>
+            <tbody>
+              <tr>
+                <th scope="row">30/12/2024</th>
+                <td>Μαρία Κουτσάκη</td>
+                <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
+                  με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
+                  Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
+              </tr>
+              <tr>
+                <th scope="row">30/12/2024</th>
+                <td>Μαρία Κουτσάκη</td>
+                <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
+                  με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
+                  Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
+              </tr>
+              <tr>
+                <th scope="row">30/12/2024</th>
+                <td>Μαρία Κουτσάκη</td>
+                <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
+                  με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
+                  Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
+              </tr>
+              <tr>
+                <th scope="row">30/12/2024</th>
+                <td>Μαρία Κουτσάκη</td>
+                <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
+                  με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
+                  Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
+              </tr>
+              <tr>
+                <th scope="row">30/12/2024</th>
+                <td>Μαρία Κουτσάκη</td>
+                <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
+                  με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
+                  Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
+              </tr>
+              <tr>
+                <th scope="row">30/12/2024</th>
+                <td>Μαρία Κουτσάκη</td>
+                <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
+                  με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
+                  Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
+              </tr>
+              <tr>
+                <th scope="row">30/12/2024</th>
+                <td>Μαρία Κουτσάκη</td>
+                <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
+                  με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
+                  Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
+              </tr>
+              <tr>
+                <th scope="row">30/12/2024</th>
+                <td>Μαρία κουτσομητροπουλος</td>
+                <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
+                  με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
+                  Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
+              </tr>
+              <tr>
+                <th scope="row">30/12/2024</th>
+                <td>Μαρία Κουτσάκη</td>
+                <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
+                  με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
+                  Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
+              </tr>
+              <tr>
+                <th scope="row">30/12/2024</th>
+                <td>Μαρία Κουτσάκη</td>
+                <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
+                  με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
+                  Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
+              </tr>
+              <tr>
+                <th scope="row">30/12/2024</th>
+                <td>Μαρία Κουτσάκη</td>
+                <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
+                  με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
+                  Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
+              </tr>
+              <tr>
+                <th scope="row">30/12/2024</th>
+                <td>Μαρία Κουτσάκη</td>
+                <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
+                  με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
+                  Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
+              </tr>
+              <tr>
+                <th scope="row">30/12/2024</th>
+                <td>Μαρία Κουτσάκη</td>
+                <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
+                  με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
+                  Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
+              </tr>
+              <tr>
+                <th scope="row">30/12/2024</th>
+                <td>Μαρία Κουτσάκη</td>
+                <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
+                  με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
+                  Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
+              </tr>
+              <tr>
+                <th scope="row">30/12/2024</th>
+                <td>Μαρία Κουτσάκη</td>
+                <td class="truncate"> Αλγόριθμοι για επιλογή σχεδόν-βέλτιστου υποσυνόλου γνωρισμάτων στην κατηγοριοποίηση
+                  με XGBoost: SHAP-based naïve αλγόριθμοι, SHAP-based non-naïve αλγόριθμοι, και η μέθοδος SHAP-based
+                  Boruta, και εφαρμογή σε δεδομένα από «Beyond-5G» τηλεπικοινωνιακά δίκτυα.</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
 
-
-        </form>
       </div>
+
+
     </div>
   </div>
   <canvas class="background"></canvas>
@@ -203,3 +212,11 @@
 <script src="./js/index/handleLogin.js"></script>
 <!-- background options -->
 <script src="./js/backgroundOptions.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+
+<script>
+  flatpickr("#calendarDate", {
+    mode: "range",
+    dateFormat: "d-m-Y",
+  });
+</script>
