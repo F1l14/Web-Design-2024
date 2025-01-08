1be94252ac6b26a73fcbb63eadcd9d6b0c1c2c1d
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2025-01-08 03:42:58 +0200

    replaced a .html with .pdf

--- php/student/scripts/manage/getPraktikoPdf.php
+++ php/student/scripts/manage/getPraktikoPdf.php
@@ -34,7 +34,7 @@ if (isset($_COOKIE["user"])) {
     $praktikaUrl = "/Web-Design-2024/Data/praktika/" . $diplomatiki . "/";
     if (file_exists($praktikaDir . "praktiko.pdf")) {
         $resp->answer = true;
-        $resp->url = $praktikaUrl . "praktiko.html";
+        $resp->url = $praktikaUrl . "praktiko.pdf";
     }
     echo json_encode($resp);
 }else{
