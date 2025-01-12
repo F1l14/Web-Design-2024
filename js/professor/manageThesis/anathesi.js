026e4a2b9f5a05399e337a02346540588175c1b7
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2025-01-13 00:07:48 +0200

    fix: remove console log for raw response in loadDetails function

--- js/professor/manageThesis/anathesi.js
+++ js/professor/manageThesis/anathesi.js
@@ -17,7 +17,7 @@ async function loadDetails() {
     })
         .then(response => {
             return response.text().then(text => {
-                console.log("Raw Response:", text);
+                // console.log("Raw Response:", text);
                 try {
                     return JSON.parse(text); // Try parsing the JSON
                 } catch (error) {
